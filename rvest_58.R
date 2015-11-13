f03ed72aadb062204f2ad7a75664cfc9b2fe7ab6
Sun Lijun <ashther@qq.com>
2015-11-13 16:51:17 +0800

    Update rvest_58.R

--- rvest_58.R
+++ rvest_58.R
@@ -14,20 +14,23 @@ urlParse <- function(url, nodes){
         iconv('utf-8', 'gbk', sub = '')
 }
 
+# s是将要被解析的字符串，包含了大量“\r\t\n”符号
+# con是用于检索的关键字 比如“期望月薪：”
 detailParse <- function(s, con){
     s_temp <- s %>% 
-        gsub('\r|\n|\t', ' ', .) %>% 
-        strsplit(' ', fixed = TRUE) %>% 
-        unlist
-    s_temp <- s_temp[s_temp != '']
-    ids <- grep(con, s_temp)
+        gsub('\r|\n|\t', ' ', .) %>%  # 首先将s中所有的“\r\n\t”替换成空格
+        strsplit(' ', fixed = TRUE) %>%  # 利用空格分割上一步得到的字符串，结果是一个含有大量空值的list
+        unlist # unlist之后得到一个含有大量空值的字符串向量，赋给s_temp
+        
+    s_temp <- s_temp[s_temp != ''] # 利用s_temp != ''创建了逻辑向量，并作为索引用于提取s_temp中的非空值
+    ids <- grep(con, s_temp) # 根据关键字con在s_temp中做检索，得到包含该con的索引，这个地方奇怪的是如果设置fixed=TRUE反而会出错
     
-    if (!identical(ids, integer(0))) {
-        result <- s_temp[ids] %>% 
-            str_replace(con, '') %>% 
-            iconv('utf-8', 'gbk', sub = '')
+    if (!identical(ids, integer(0))) { # 因为当检索不到con时 ids会等于integer(0)，所以ids不等于integer(0)时执行以下步骤
+        result <- s_temp[ids] %>%  # 以ids为索引提取s_temp中的元素，这个元素就是包含con的元素
+            str_replace(con, '') %>%  # 将该元素中的con部分替换为空，比如“期望月薪：面议”变为“面议”
+            iconv('utf-8', 'gbk', sub = '') # 处理编码格式的
     } else {
-        result <- NA
+        result <- NA # 如果ids为integer(0)，说明s_temp中没有含有con的元素，也就是这份简历的该字段缺失，赋值为NA
     }
     return(result)
 }
