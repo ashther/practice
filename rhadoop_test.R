0d241f70da316a9a814643546f95487a309383a7
Sun Lijun <ashther@qq.com>
2015-12-14 23:12:30 +0800

    Update rhadoop_test.R

--- rhadoop_test.R
+++ rhadoop_test.R
@@ -2,7 +2,26 @@ library(rhdfs)
 library(rmr2)
 
 hdfs.init()
-
+#===================
 small.ints <- to.dfs(1:10)
-mapreduce(input = small.ints, map = function(k, v)cbind(v, v^2))
-from.dfs('/tmp/file92e6f53a2d8') #ajust
+mapreduce(input = small.ints, map = function(k, v)cbind(k, v))
+from.dfs('/tmp/file19f73df21734') #ajust
+#===================
+webpages <- read.csv('webpages_mapreduce.csv', stringsAsFactors = FALSE)
+totalvisits <- sum(webpages$visits)
+
+webpages.hdfs <- to.dfs(webpages)
+wp.mapper1 <- function(k, v){
+  key <- v[1]
+  val <- v[3]
+  keyval(key, val)
+}
+
+wp.reducer1 <- function(k, v){
+  per <- sum(v) / totalvisits * 100
+  keyval(k, per)
+}
+
+mapreduce(input = webpages.hdfs, map = wp.mapper1, reduce = wp.reducer1)
+result <- from.dfs('/tmp/file19f71ab2f8ee')
+
