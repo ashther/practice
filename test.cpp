a3ecf51eb9a24fca16c5c1efa83138434be22d55
Sun Lijun <ashther@qq.com>
2015-12-29 08:25:32 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include <Rcpp.h>
+#include <iostream>
 using namespace Rcpp;
+using namespace std;
 
 // This is a simple example of exporting a C++ function to R. You can
 // source this function into an R session using the Rcpp::sourceCpp 
@@ -32,6 +34,60 @@ bool allC(LogicalVector x) {
     return true;
 }
 
+// [[Rcpp::export]]
+NumericVector cumsumC(NumericVector x) {
+    int n = x.size();
+    double temp = 0;
+    NumericVector result(n);
+    for (int i = 0; i < n; i++) {
+        temp += x[i];
+        //cout<<temp<<endl;
+        result[i] = temp;
+    }
+    return result;
+}
+
+// [[Rcpp::export]]
+NumericVector cumprodC(NumericVector x) {
+    int n = x.size();
+    double temp = 1;
+    NumericVector result(n);
+    for (int i = 0; i < n; i++) {
+        temp *= x[i];
+        result[i] = temp;
+    }
+    return result;
+}
+
+// [[Rcpp::export]]
+NumericVector cumminC(NumericVector x) {
+    int n = x.size();
+    NumericVector result(1);
+    result[0] = x[0];
+    for (int i = 1; i < n; i++) {
+        if (x[i] <= min(result)) {
+            result.push_back(x[i]);
+        } else {
+            result.push_back(result[i-1]);
+        }
+    }
+    return result;
+}
+
+// [[Rcpp::export]]
+NumericVector cummaxC(NumericVector x) {
+    int n = x.size();
+    NumericVector result(1);
+    result[0] = x[0];
+    for (int i = 1; i < n; i++) {
+        if (x[i] >= max(result)) {
+            result.push_back(x[i]);
+        } else {
+            result.push_back(result[i-1]);
+        }
+    }
+    return result;
+}
 
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically 
@@ -42,6 +98,23 @@ bool allC(LogicalVector x) {
 require(microbenchmark)
 x_mean <- runif(1e3)
 microbenchmark(meanC(x_mean), mean(x_mean))
+
 x_all <- 1:10
-allC(x_all < 15)
+microbenchmark(all(x_all < 15), allC(x_all < 15))
+
+x_cumsum <- 1:10
+microbenchmark(cumsum(x_cumsum), cumsumC(x_cumsum))
+
+x_cumprod <- 1:10
+microbenchmark(cumprod(x_cumprod), cumprodC(x_cumprod))
+
+x_cummin <- c(3:1, 2:0, 4:2)
+microbenchmark(cummin(x_cummin), cumminC(x_cummin))
+
+x_cummax <- c(3:1, 2:0, 4:2)
+microbenchmark(cummax(x_cummax), cummaxC(x_cummax))
 */
+
+
+
+
