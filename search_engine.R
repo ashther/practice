1b601ff95ba79369314ee64e802d6a56fcd67b83
Sun Lijun <ashther@qq.com>
2016-10-18 15:38:22 +0800

    加入了离线计算和新文档计算部分

--- search_engine.R
+++ search_engine.R
@@ -1,6 +1,7 @@
 library(tm)
 library(SnowballC)
 library(magrittr)
+library(dplyr)
 
 doc1 <- "Stray cats are running all over the place. I see 10 a day!"
 doc2 <- "Cats are killers. They kill billions of animals a year."
@@ -10,42 +11,148 @@ doc5 <- "Buy Brand C cat food for your cat. Brand C makes healthy and happy cats
 doc6 <- "The Arnold Classic came to town this weekend. It reminds us to be healthy."
 doc7 <- "I have nothing to say. In summary, I have told you nothing."
 
-doc_list <- eval(parse(text = sprintf("list(%s)", paste0('doc', 1:7, collapse = ','))))
-N_docs <- length(doc_list)
-names(doc_list) <- paste0('doc', 1:N_docs)
-
 query <- "Healthy cat food"
 
-my_corpus <- VectorSource(c(doc_list, query))
-my_corpus$Names <- c(names(doc_list), 'query')
-my_corpus <- Corpus(my_corpus)
+set.seed(2016)
+doc <- data.frame(
+  id = sample.int(10, size = 7), 
+  content = eval(parse(text = sprintf("c(%s)", paste0('doc', 1:7, collapse = ',')))), 
+  stringsAsFactors = FALSE, row.names = NULL
+)
 
-my_corpus <- my_corpus %>% 
-  tm_map(removePunctuation) %>% 
-  tm_map(stemDocument) %>% 
-  tm_map(removeNumbers) %>% 
-  tm_map(content_transformer(tolower)) %>% 
-  tm_map(stripWhitespace)
+new_doc <- data.frame(
+  id = 11, 
+  content = "A new brand cat food? wow, we got let the ketty have a try the food.", 
+  stringsAsFactors = FALSE, row.names = NULL
+)
 
-term_doc_matrix_stm <- TermDocumentMatrix(my_corpus)
-term_doc_matrix <- as.matrix(term_doc_matrix_stm)
+#  data 
 
-termWeight <- function(tf_vec, df) {
-  result <- rep(0, length(tf_vec))
-  result[tf_vec > 0] <- (1 + log2(tf_vec[tf_vec > 0])) * log2(length(tf_vec) / df)
+dfToCorpus <- function(df) {
+  myReader <- readTabular(mapping = list(content = 'content', id = 'id'))
+  VCorpus(DataframeSource(df), readerControl = list(reader = myReader))
+}
+
+corpusToMtx <- function(corpus) {
+  temp <- corpus %>% 
+    tm_map(removePunctuation) %>%
+    tm_map(removeNumbers) %>%
+    tm_map(content_transformer(tolower)) %>%
+    tm_map(removeWords, stopwords('english')) %>%
+    tm_map(stripWhitespace) %>%
+    tm_map(stemDocument)
+  
+  result <- TermDocumentMatrix(temp) %>% 
+    as.matrix()
+  
   return(result)
 }
 
-matrixWeightScale <- function(mtx) {
-  mtx_weighted_scaled <- apply(mtx, 1, function(x) {
-    termWeight(x, sum(x > 0))
+termWeight <- function(tf_vec) {
+  result <- rep(0, length(tf_vec))
+  result[tf_vec > 0] <- (1 + log2(tf_vec[tf_vec > 0])) * 
+                        log2(length(tf_vec) / sum(tf_vec > 0))
+  return(result)
+}
+
+matrixWeightScale <- function(doc) {
+  tdmtx <- doc %>% 
+    dfToCorpus() %>% 
+    corpusToMtx()
+  
+  mtx_weighted_scaled <- apply(tdmtx, 1, function(x) {
+    termWeight(x)
   }) %>% 
     t() %>% 
-    set_colnames(colnames(mtx)) %>% 
+    set_colnames(colnames(tdmtx)) %>% 
     scale(center = FALSE, scale = sqrt(colSums(.^2)))
   return(mtx_weighted_scaled)
 }
 
-querySearch <- function(query, mtx) {
+newDocWeightScale <- function(new_doc, tdmtx) {
   
+  term <- rownames(tdmtx)
+  new_doc_mtx <- new_doc %>% 
+    dfToCorpus() %>% 
+    corpusToMtx()
+  
+  temp <- sapply(rownames(tdmtx), function(x) {
+    if (!x %in% rownames(new_doc_mtx)) {
+      return(0)
+    } else {
+      (1 + log2(new_doc_mtx[x, ])) * log2(ncol(tdmtx) / sum(tdmtx[x, ] > 0))
+    }
+  }) %>% 
+    as.matrix() %>% 
+    set_colnames(new_doc$id) %>% 
+    scale(center = FALSE, scale = sqrt(colSums(.^2)))
+  return(cbind(tdmtx, temp))
 }
+
+querySearch <- function(query, tdmtx, doc, k = 10, thr = 0) {
+  stopifnot(is.numeric(k), 
+            is.numeric(thr), 
+            is.character(query), 
+            is.matrix(tdmtx))
+  
+  term <- rownames(tdmtx)
+  
+  query_mtx <- VectorSource(query) %>% 
+    Corpus() %>% 
+    corpusToMtx()
+  
+  temp <- matrix(0, 1, nrow(tdmtx), dimnames = list('query', term))
+  temp[, term %in% rownames(query_mtx)] <- query_mtx[, 1]
+  
+  searched <- temp %*% tdmtx %>% 
+    extract(1, ) %>% 
+    Filter(function(x)x >= thr, .)
+  
+  if (length(searched) == 0) {
+    return(NULL)
+  }
+  
+  result <- data.frame(
+    id = names(searched)[
+      order(searched, decreasing = TRUE)
+    ] %>% as.integer()
+  ) %>% 
+    left_join(doc, 'id')
+  
+  if (k < 0) {
+    return(result$content)
+  } 
+  
+  return(slice(result, seq_len(k))$content)
+}
+
+# run daily
+tdmtx <- matrixWeightScale(doc)
+
+# # run whenever new doc created
+# new_tdmtx <- newDocWeightScale(new_doc, new_tdmtx) 
+# 
+# # run while searching
+# querySearch(query, new_tdmtx, rbind(doc, new_doc), 5, 0.2) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
