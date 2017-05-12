a8e7c4380aee0a435bd849a130063ee771dedea4
Sun Lijun <ashther@qq.com>
2017-05-12 14:39:06 +0800

    Update fisherCluster.R

--- fisherCluster/fisherCluster.R
+++ fisherCluster/fisherCluster.R
@@ -33,31 +33,7 @@ dMtxCreate <- function(seq_data) {
   return(dMtx)
 }
 
-minLossSplit <- function(dMtx, n, k) {
-  j_seq <- seq(k, n)
-  if (k == 2L) {
-    result <- sapply(j_seq, function(j) {
-      if (1 == (j - 1)) {
-        return(dMtx[as.character(n), as.character(j)])
-      } else if (n == j) {
-        return(dMtx[as.character(j - 1), '1'])
-      } else {
-        return(dMtx[as.character(j - 1), '1'] + dMtx[as.character(n), as.character(j)])
-      }
-    })
-  } else {
-    result <- sapply(j_seq, function(j) {
-      if (j == n) {
-        return(minLossSplit(dMtx, j - 1, k - 1)$value)
-      } else {
-        return(minLossSplit(dMtx, j - 1, k - 1)$value + dMtx[as.character(n), as.character(j)])
-      }
-    })
-  }
-  return(list(value = min(result), idx = j_seq[which.min(result)]))
-}
-
-minLossSplitTemp <- function(dMtx, min_loss_mtx, n, k) {
+minLossSplit <- function(dMtx, min_loss_mtx, n, k) {
   j_seq <- seq(k, n)
   if (k == 2L) {
     result <- sapply(j_seq, function(j) {
@@ -92,15 +68,30 @@ lossMtxCreate <- function(dMtx) {
   on.exit(closepb(pb))
   for(k in colnames(min_loss_mtx)) {
     for(n in rownames(min_loss_mtx)[which(colnames(min_loss_mtx) == k):length(rownames(min_loss_mtx))]) {
-      # min_loss <- minLossSplit(dMtx, as.integer(n), as.integer(k))
+      # min_loss <- minLossSplitRcr(dMtx, as.integer(n), as.integer(k))
       # min_loss_mtx[n, k] <- min_loss$value
       # min_loss_idx[n, k] <- min_loss$idx
-      # 
-      # min_loss <- minLossSplitCpp(dMtx, as.integer(n), as.integer(k))
-      # min_loss_mtx[n, k] <- min(min_loss)
-      # min_loss_idx[n, k] <- which.min(min_loss) + which(colnames(min_loss_mtx) == k)
       
-      min_loss <- minLossSplitTemp(dMtx, min_loss_mtx, as.integer(n), as.integer(k))
+      min_loss <- minLossSplit(dMtx, min_loss_mtx, as.integer(n), as.integer(k))
+      min_loss_mtx[n, k] <- min_loss$value
+      min_loss_idx[n, k] <- min_loss$idx
+    }
+    setpb(pb, which(k == colnames(min_loss_mtx)))
+  }
+  return(list(value = min_loss_mtx, idx = min_loss_idx))
+}
+
+lossMtxCreateCpp <- function(dMtx) {
+  require(pbapply)
+  len <- nrow(dMtx) - 1
+  min_loss_mtx <- matrix(0, len, len, dimnames = list(rownames(dMtx)[-1], colnames(dMtx)[-1]))
+  min_loss_idx <- min_loss_mtx
+  
+  pb <- startpb(0, len)
+  on.exit(closepb(pb))
+  for(k in colnames(min_loss_mtx)) {
+    for(n in rownames(min_loss_mtx)[which(colnames(min_loss_mtx) == k):length(rownames(min_loss_mtx))]) {
+      min_loss <- minLossSplitCpp(dMtx, min_loss_mtx, as.integer(n), as.integer(k))
       min_loss_mtx[n, k] <- min_loss$value
       min_loss_idx[n, k] <- min_loss$idx
     }
@@ -126,13 +117,13 @@ fisherClust <- function(lossMtx, k = 3) {
     c_start <- lossMtx$idx[n, as.character(k)]
     if (c_start == 2L) {
       cluster[[as.character(k)]] <- c('2', name_mtx[1:which(name_mtx == n)])
-      # loss <- loss + lossMtx$value[n, '2']
+      loss <- loss + lossMtx$value[n, as.character(k)]
       cluster[['1']] <- '1'
       break
     } else {
       cluster[[as.character(k)]] <- 
         name_mtx[which(name_mtx == as.character(c_start)):which(name_mtx == n)]
-      # loss <- loss + lossMtx$value[n, as.character(k)]
+      loss <- loss + lossMtx$value[n, as.character(k)]
       k <- k - 1
       n <- name_mtx[which(name_mtx == as.character(c_start)) - 1]
       if (k == 1L) {
@@ -145,5 +136,29 @@ fisherClust <- function(lossMtx, k = 3) {
       }
     }
   }
-  return(cluster)
+  return(list(cluster = cluster, loss = loss))
 }
+
+# minLossSplitRcr <- function(dMtx, n, k) {
+#   j_seq <- seq(k, n)
+#   if (k == 2L) {
+#     result <- sapply(j_seq, function(j) {
+#       if (1 == (j - 1)) {
+#         return(dMtx[as.character(n), as.character(j)])
+#       } else if (n == j) {
+#         return(dMtx[as.character(j - 1), '1'])
+#       } else {
+#         return(dMtx[as.character(j - 1), '1'] + dMtx[as.character(n), as.character(j)])
+#       }
+#     })
+#   } else {
+#     result <- sapply(j_seq, function(j) {
+#       if (j == n) {
+#         return(minLossSplit(dMtx, j - 1, k - 1)$value)
+#       } else {
+#         return(minLossSplit(dMtx, j - 1, k - 1)$value + dMtx[as.character(n), as.character(j)])
+#       }
+#     })
+#   }
+#   return(list(value = min(result), idx = j_seq[which.min(result)]))
+# }
