579512dadb36e59e05843ef29e61147d187af515
Sun Lijun <ashther@qq.com>
2017-05-26 11:36:57 +0800

    Update dataSource.R

--- busTrans/dataSource.R
+++ busTrans/dataSource.R
@@ -1,5 +1,6 @@
+
 library(igraph)
-mat <- as.matrix(read.table(text=
+matTime <- as.matrix(read.table(text=
                               "node X1 X2 X3 X4 X5 X6
                             1  0  3  7  4 NA NA
                             2  3  0  2 NA NA  9
@@ -7,7 +8,14 @@ mat <- as.matrix(read.table(text=
                             4  4 NA  1  0  3 NA
                             5 NA NA  3  3  0  3
                             6 NA  9  6 NA  3  0", header=T))
-rownames(mat) <- mat[,1]
-mat <- mat[, -1]
-colnames(mat) <- rownames(mat)
-mat[is.na(mat)] <- 0
+rownames(matTime) <- matTime[,1]
+matTime <- matTime[, -1]
+colnames(matTime) <- rownames(matTime)
+matTime[is.na(matTime)] <- 0
+
+mat <- matTime
+mat[mat > 0] <- 1
+
+matTrans <- matTime
+matTrans <- matTrans * 0
+matTrans[rbind(c('4', '3'), c('5', '6'))] <- 1
