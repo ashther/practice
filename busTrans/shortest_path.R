b365641ebfde17ab3b6de03528dc94f4569d24df
Sun Lijun <ashther@qq.com>
2017-05-31 17:59:22 +0800

    Update shortest_path.R

--- busTrans/shortest_path.R
+++ busTrans/shortest_path.R
@@ -84,24 +84,26 @@ spFindTopKResume <- function(mat, from, to, k, result = list(), variants = list(
   return(list(result = result, variants = variants))
 }
 
-variantCaculate <- function(variants, variant, from, to) {
-  g <- variant$g
-  for (j in unlist(variant$path)) {
-    new_g <- delete.edges(g, j)
-    sp <- suppressWarnings2(
-      get.shortest.paths(new_g, from, to, mode = 'out', output = 'both'), 
-      regex = "Couldn't reach some vertices"
-    )
-    spd <- shortest.paths(new_g, from, to, mode = 'out', algorithm = 'dijkstra')
-    if (!is.infinite(spd)) {
-      if (!spContains(variants, sp)) {
-        variants[[length(variants) + 1]] <- 
-          list(g = new_g, vert = sp$vpath, path = sp$epath, dist = spd)
+variantCaculate <- memoise(
+  function(variants, variant, from, to) {
+    g <- variant$g
+    for (j in unlist(variant$path)) {
+      new_g <- delete.edges(g, j)
+      sp <- suppressWarnings2(
+        get.shortest.paths(new_g, from, to, mode = 'out', output = 'both'), 
+        regex = "Couldn't reach some vertices"
+      )
+      spd <- shortest.paths(new_g, from, to, mode = 'out', algorithm = 'dijkstra')
+      if (!is.infinite(spd)) {
+        if (!spContains(variants, sp)) {
+          variants[[length(variants) + 1]] <- 
+            list(g = new_g, vert = sp$vpath, path = sp$epath, dist = spd)
+        }
       }
     }
+    return(variants)
   }
-  return(variants)
-}
+)
 
 spContains <- function(variants, sp) {
   any(unlist(lapply(variants, function(x)identical(unlist(x$vert), unlist(sp$vpath)))))
@@ -126,17 +128,18 @@ mtxValueGet <- function(vec, mat) {
 # sp <- spFindTopKResume()
 spFilter <- function(sp, n, matTrans) {
   idx <- sapply(sp$result, function(x) {
-    temp <- unlist(x$vert)
+    temp <- names(unlist(x$vert))
     temp <- temp[-c(1, length(temp))]
-    (length(temp) <= n) | ((length(temp) <= (n + 1)) & any(mtxValueGet(temp, matTrans) == 1))
+    (length(temp) <= n) | 
+      ((length(temp) <= (n + 1)) & any(mtxValueGet(temp, matTrans) == 1))
   })
-  lapply(sp$result[idx], function(x)list(vert = unname(unlist(x$vert)), dist = x$dist))
+  lapply(sp$result[idx], function(x)list(vert = unlist(x$vert), dist = x$dist))
 }
 
 # sp <- spFilter()
 spSort <- function(sp, matTime, matTrans, by = c('tran', 'time')) {
   time_tran <- t(sapply(sp, function(x) {
-    vec <- x$vert
+    vec <- names(x$vert)
     time <- sum(mtxValueGet(vec, matTime))
     tran <- length(vec) - sum(mtxValueGet(vec, matTrans)) - 2
     # tran <- length(vec) - sum(mtxValueGet(vec[-c(1, length(vec))], matTrans)) - 2
@@ -144,7 +147,7 @@ spSort <- function(sp, matTime, matTrans, by = c('tran', 'time')) {
   }))
   orders <- order(time_tran[, by[1]], time_tran[, by[2]])
   result <- lapply(seq_along(sp), function(x) {
-    c(paste0(sp[[x]]$vert, collapse = ','),
+    c(paste0(names(sp[[x]]$vert), collapse = ','),
       time_tran[x, 'tran'],
       time_tran[x, 'time'])
   }) %>% 
@@ -155,3 +158,55 @@ spSort <- function(sp, matTime, matTrans, by = c('tran', 'time')) {
   result$time <- as.numeric(result$time)
   return(result[orders, ])
 }
+
+# sp <- spSort()
+spTranslate <- function(sp, dfLines) {
+  sp$lines <- apply(sp, 1, function(x) {
+    temp <- unlist(strsplit(x[1], ','))
+    temp <- cbind(temp[1:(length(temp) - 1)], 
+                  temp[2:length(temp)])
+    temp <- apply(temp, 1, function(x) {
+      stationBetween(dfLines, x[1], x[2], loop_lines = c('2', '10')) %>% {
+        names(.)[which.min(.)]
+      }
+    })
+    if (length(temp) == 1) {
+      return(paste0(temp, collapse = ','))
+    } else if (any(temp[1:(length(temp) - 1)] == temp[2:length(temp)])) {
+      return(NA)
+    } else {
+      return(paste0(temp, collapse = ','))
+    }
+  })
+  distinct(filter(sp, !is.na(lines)))
+}
+
+start <- Sys.time()
+sp <- spFindTopKResume(mat, '北苑', '安定门', 15)
+cat(Sys.time() - start)
+start <- Sys.time()
+sp <- spFilter(sp, 2, matTrans)
+cat(Sys.time() - start)
+start <- Sys.time()
+sp <- spSort(sp, matTime, matTrans)
+cat(Sys.time() - start)
+start <- Sys.time()
+sp <- spTranslate(sp, dfLines)
+cat(Sys.time() - start)
+
+pbapply::pblapply(unique(dfLines$station), FUN = function(x) {
+  lapply(unique(dfLines$station), function(y) {
+    if (x == y) {
+      return(NA)
+    }
+    sp <- spFindTopKResume(mat, x, y, 10)
+    sp <- spFilter(sp, 2, matTrans)
+    if (length(sp) <= 0) {
+      return(NA)
+    }
+    sp <- spSort(sp, matTime, matTrans) 
+    spTranslate(sp, dfLines)
+  })
+}) %>% 
+  invisible()
+
