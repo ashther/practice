45177e92010283c53922abed579000beac37dfee
Sun Lijun <ashther@qq.com>
2017-06-02 12:03:56 +0800

    Update parallel.R

--- busTrans/parallel.R
+++ busTrans/parallel.R
@@ -200,26 +200,18 @@ stationBetween <- function(dfLines, i, j, loop_lines = NULL) {
   })
 }
 
-stopBetweenTest <- function(listLines, from, to, lines, loop_lines) {
-  sapply(lines, function(l) {
-    stop <- listLines[[l]]
-    if (l %in% loop_lines) {
-      min(
-        abs(which(stop == from) - which(stop == to)), 
-        (length(stop) - abs(which(stop == from) - which(stop == to)))
-      )
-    } else {
-      abs(which(stop == from) - which(stop == to))
-    }
-  })
+stopFilter <- function(dfLines, from_to, to, stops, p = 1) {
+  to_lon <- dfLines[dfLines$station == to, 2][1]
+  to_lat <- dfLines[dfLines$station == to, 3][1]
+  stops_lon <- unique(dfLines[dfLines$station %in% stops, 2])
+  stops_lat <- unique(dfLines[dfLines$station %in% stops, 3])
+  (abs(stops_lon - to_lon) + abs(stops_lat - to_lat)) <= from_to*p
 }
 
-
-
-reachFind <- function(mat, from, to, matLines, listLines) {
+reachFind <- function(mat, from, to, matLines, matTime) {
   if (mat[from, to] == 1) {
     lines <- rownames(matLines)[matLines[, from] == 1 & matLines[, to] == 1]
-    time <- stopBetweenTest(listLines, from, to, lines, loop_lines = c('2', '10'))
+    time <- sapply(lines, function(x)matTime[[x]][from, to])
     return(
       matrix(c(rep(paste(from, to, sep = ','), length(lines)), 
                lines, rep(0, length(lines)), unname(time)), 
@@ -231,14 +223,17 @@ reachFind <- function(mat, from, to, matLines, listLines) {
 }
 
 
-
-tran1Find <- function(mat, from, to, matLines, listLines) {
+tran1Find <- function(mat, from, to, matLines, matTime, dfLines) {
   stops <- rownames(mat)[mat[from, ] == 1 & mat[, to] == 1]
+  from_to <- sum(abs(dfLines[dfLines$station == from, 2:3][1, ] - 
+                       dfLines[dfLines$station == to, 2:3][1, ]))
+  stops <- stops[stopFilter(dfLines, from_to, to, stops, p = 1)]
   do.call(rbind, 
           lapply(stops, function(x) {
-            line_a <- reachFind(mat, from, x, matLines, listLines)
-            line_b <- reachFind(mat, x, to, matLines, listLines)
-            temp <- as.matrix(expand.grid(line_a[, 'line'], line_b[, 'line']))
+            line_a <- reachFind(mat, from, x, matLines, matTime)
+            line_b <- reachFind(mat, x, to, matLines, matTime)
+            temp <- expand.grid(line_a[, 'line'], line_b[, 'line'], 
+                                stringsAsFactors = FALSE)
             temp <- temp[temp[, 1] != temp[, 2], , drop = FALSE]
             if (nrow(temp) == 0) {
               return(NULL)
@@ -254,21 +249,33 @@ tran1Find <- function(mat, from, to, matLines, listLines) {
           }))
 }
 
+############################################################
+#                                                          #
+#                            2                             #
+#                                                          #
+############################################################
 
+# from <- '北苑'
+# to <- '安定门'
 
-tran2Find <- function(mat, from, to, matLines, listLines) {
+tran2Find <- function(mat, from, to, matLines, matTime, dfLines) {
   stop_from <- rownames(mat)[mat[from, ] == 1]
   stop_to <- rownames(mat)[mat[, to] == 1]
   stop_combn <- as.matrix(expand.grid(stop_from, stop_to))
   stop_combn <- stop_combn[mat[stop_combn] == 1, ]
+  
+  from_to <- sum(abs(dfLines[dfLines$station == from, 2:3][1, ] - 
+                       dfLines[dfLines$station == to, 2:3][1, ]))
+  stop_combn <- stop_combn[stopFilter(dfLines, from_to, to, stop_combn[, 1], p = 1), ]
+  
   do.call(rbind, 
           lapply(seq_len(nrow(stop_combn)), function(x) {
-            line_a <- reachFind(mat, from, stop_combn[x, 1], matLines, listLines)
-            line_b <- reachFind(mat, stop_combn[x, 1], stop_combn[x, 2], matLines, listLines)
-            line_c <- reachFind(mat, stop_combn[x, 2], to, matLines, listLines)
-            temp <- as.matrix(expand.grid(line_a[, 'line'], 
-                                          line_b[, 'line'], 
-                                          line_c[, 'line']))
+            line_a <- reachFind(mat, from, stop_combn[x, 1], matLines, matTime)
+            line_b <- reachFind(mat, stop_combn[x, 1], stop_combn[x, 2], matLines, matTime)
+            line_c <- reachFind(mat, stop_combn[x, 2], to, matLines, matTime)
+            temp <- expand.grid(line_a[, 'line'], 
+                                line_b[, 'line'], 
+                                line_c[, 'line'], stringsAsFactors = FALSE)
             temp <- temp[temp[, 1] != temp[, 2] & 
                            temp[, 2] != temp[, 3], , drop = FALSE]
             if (nrow(temp) == 0) {
