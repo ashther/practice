b6baa0dac7e2b66ddf4846510e0d0e4f25e70250
Sun Lijun <ashther@qq.com>
2017-08-05 09:47:52 +0800

    Update dataSource.R

--- script_sna/dataSource.R
+++ script_sna/dataSource.R
@@ -4,6 +4,7 @@ library(stringr)
 library(tibble)
 library(magrittr)
 
+# ropencc::converter(type = ropencc::T2S)['開放中文轉換']
 script <- readLines('data/data.txt') %>% 
   str_subset('[^^$]')
 script <- data_frame(id = seq_along(script), text = script) %>% 
@@ -25,16 +26,18 @@ script <- data_frame(id = seq_along(script), text = script) %>%
 #     script[idx - 1, c('is_scene', 'is_dialogue', 'people', 'is_intro', 'is_title')]
 #   idx <- which(with(script, is_scene + is_dialogue + is_intro + is_title) == 0)
 # }
-# 
-# # after scene loc finished
-# script$loc <- c(NA,
-#                 rep(scene$loc, sapply(unique(script$scene[-1]), function(x) {
-#                   sum(script$scene == x)
-#                 })))
+
+
 
 people <- script %>% 
   filter(nchar(people) > 0) %>% 
   select(id, scene, episode, people)
+people <- people %>% 
+  group_by(people, episode, scene) %>% 
+  tally() %>% 
+  left_join(people, ., by = c('people' = 'people', 
+                              'scene' = 'scene', 
+                              'episode' = 'episode'))
 
 require(jiebaR)
 tag <- worker('tag', stop_word = 'dict/stop_words.utf8', bylines = TRUE)
@@ -61,3 +64,30 @@ scene$loc <- tagging(scene$text, tag) %>%
     paste0(x, collapse = '')
   })
 
+# after scene loc finished
+script$loc <- c(NA,
+                rep(scene$loc, sapply(unique(script$scene[-1]), function(x) {
+                  sum(script$scene == x)
+                })))
+
+seg <- worker(stop_word = 'dict/stop_words.utf8', bylines = TRUE)
+script_term <- segment(script$text, seg) %>% {
+  data_frame(id = rep(script$id, sapply(., length)), word = unlist(.))
+}
+
+sentiment <- read.csv('dict/sentiment.csv', na.strings = '', stringsAsFactors = FALSE)
+sentiment <- sentiment %>% 
+  select(-meaning_No, -meaning_sum, -sentiment2, -strength2, -polarity2) %>% 
+  mutate(sentiment0 = case_when(
+    sentiment %in% c('PA', 'PE') ~ 'happy', 
+    sentiment %in% c('PD', 'PH', 'PG', 'PB', 'PK') ~ 'good',
+    sentiment %in% c('NA') ~ 'angry',
+    sentiment %in% c('NB', 'NJ', 'NH', 'PF') ~ 'sad',
+    sentiment %in% c('NI', 'NC', 'NG') ~ 'fear',
+    sentiment %in% c('NE', 'ND', 'NN', 'NK', 'NL') ~ 'disgust',
+    sentiment %in% c('PC') ~ 'surprise'
+  ), polarity = case_when(
+    polarity == 2 ~ -1, 
+    polarity == 3 ~ 0, 
+    TRUE ~ as.numeric(polarity)
+  ))
